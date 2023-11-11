89e88b9c3a6fe1ef72a149ce649a54159a2401d7
Mr. White <124844226+MrWhite1878@users.noreply.github.com>
2023-11-11 08:27:02 -0500

    Update ai.py

--- gameFiles/ai.py
+++ gameFiles/ai.py
@@ -1,6 +1,8 @@
 import gameFiles.game_logic as game_logic
 
+# It is named CPU because it is using an algo, not a neural network
 class CPU:
+
     # evaluation values for the minimax algorithm
     evaluation_values = {
         "X": 1,
@@ -11,16 +13,10 @@ class CPU:
     negative_infinity = float("-inf")
 
     def __init__(self, player):
-        '''
-        player: the player the AI is playing as
-        '''
         self.player = player
     
     # generates a list of all possible moves
     def possibleMoves(self, board):
-        '''
-        board: the current board state
-        '''
         empty_cells = []
         for row in range(3):
             for col in range(3):
@@ -28,57 +24,69 @@ class CPU:
                     empty_cells.append((row, col))
         return empty_cells
 
-    # returns the best move for the ai
+    # returns the CPU's move
     def getMove(self, board):
-        '''
-        board: the current board state
-        '''
         empty_cells = self.possibleMoves(board)
+
         if self.player == "X":
             best_value = self.negative_infinity
             best_move = empty_cells[0]
+
             for move in empty_cells:
                 board[move[0]][move[1]] = self.player
-                value = self.minimax(board, 9, "O")
+                value = self.minimax(board, 9, "O", self.negative_infinity, self.infinity)
                 board[move[0]][move[1]] = " "
                 if value > best_value:
                     best_value = value
                     best_move = move
+
         elif self.player == "O":
             best_value = self.infinity
             best_move = empty_cells[0]
+
             for move in empty_cells:
                 board[move[0]][move[1]] = self.player
-                value = self.minimax(board, 9, "X")
+                value = self.minimax(board, 9, "X", self.negative_infinity, self.infinity)
                 board[move[0]][move[1]] = " "
                 if value < best_value:
                     best_value = value
                     best_move = move
+
         return best_move
 
-    # minimax algorithm
-    def minimax (self, board, depth, player):
+    # minimax algorithm with fail-soft alpha-beta pruning
+    def minimax (self, board, depth, player, alpha, beta):
         '''
         board: the current board state
         depth: the current depth of the tree
         player: the current player
+        alpha: the minimum score that the maximizing player is assured of
+        beta: the maximum score that the minimizing player is assured of
         '''
         if depth == 0 or game_logic.checkWin(board):
             return self.evaluation_values[game_logic.checkWin(board)]
         
         children = self.possibleMoves(board)
         
+        # X is the maximizing player
         if player == "X":
             value = self.negative_infinity
             for child in children:
                 board[child[0]][child[1]] = player
-                value = max(value, self.minimax(board, depth - 1, "O"))
+                value = max(value, self.minimax(board, depth - 1, "O", alpha, beta))
                 board[child[0]][child[1]] = " "
+                alpha = max(alpha, value)
+                if value >= beta:
+                    break
             return value
+        
         else:
             value = self.infinity
             for child in children:
                 board[child[0]][child[1]] = player
-                value = min(value, self.minimax(board, depth - 1, "X"))
+                value = min(value, self.minimax(board, depth - 1, "X", alpha, beta))
                 board[child[0]][child[1]] = " "
+                beta = min(beta, value)
+                if value <= alpha:
+                    break
             return value
